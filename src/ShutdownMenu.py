520938a75d3ef15f108f2baaa048eccec47da29c
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-01-29 11:25:49 +0300

    fixes and translation

--- src/ShutdownMenu.py
+++ src/ShutdownMenu.py
@@ -25,10 +25,11 @@ class ShutdownMenu:
         window = Gtk.Window()
         window.connect("destroy", Gtk.main_quit)
         window.set_skip_taskbar_hint(True)
+        window.set_title(_("Eta Shutdown"))
 
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         box.set_spacing(18)
-        box.pack_start(self.create_button("eta-shutdownr-poweroff", _("Power Off"), ["pkexec", ACTION, "reboot"]), True, True, 0)
+        box.pack_start(self.create_button("eta-shutdown-poweroff", _("Power Off"), ["pkexec", ACTION, "reboot"]), True, True, 0)
         box.pack_start(self.create_button("eta-shutdown-reboot", _("Restart"), ["pkexec", ACTION, "poweroff"]), True, True, 0)
         box.pack_start(self.create_button("eta-shutdown-logout", _("Log Out"), ["pkill","-KILL", "-u", os.environ["USER"]]), True, True, 0)
 
